0633ea059da3564d8be704d80281437a43ce61e6
hdd2k <hank.lee.qed@gmail.com>
2023-03-07 17:18:43 -0800

    Add testing skeleton for Value class

---
+++ micrograd/tests/test_value.py
@@ -0,0 +1,21 @@
+# perform tests by comparing results with PyTorch equivalent
+
+import torch
+from micrograd.micrograd.value import Value
+
+
+def test_sanity_check():
+
+    # check - forward pass (gradient descent)
+    # check - backward pass (backpropagation)
+    pass
+
+
+def test_more_ops():
+    # check - forward pass (gradient descent)
+    # check - backward pass (backpropagation)
+    pass
+
+
+test_sanity_check()
+test_more_ops()
